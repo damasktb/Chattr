e11173d7247863e458c7df7ad1d5670c3a2a3fd0
Damask Talary-Brown <damasktb@gmail.com>
2015-08-05 14:57:42 +0100

    Basic chat activity for single user with callbacks

---
+++ app/src/main/java/com/damask/chattr/ChatMessage.java
@@ -0,0 +1,37 @@
+package com.damask.chattr;
+
+/**
+ * Created by demouser on 8/5/15.
+ */
+public class ChatMessage {
+
+    private String mUser;
+    private String mText;
+    private Long mTimeStamp;
+    public ChatMessage(String mUser, String mText) {
+        this.mUser = mUser;
+        this.mText = mText;
+    }
+    public String getmUser() {
+        return mUser;
+    }
+    public void setmUser(String mUser) {
+        this.mUser = mUser;
+    }
+    public String getmText() {
+        return mText;
+    }
+    public void setmText(String mText) {
+        this.mText = mText;
+    }
+    public Long getmTimeStamp() {
+        return mTimeStamp;
+    }
+    public void setmTimeStamp(Long mTimeStamp) {
+        this.mTimeStamp = mTimeStamp;
+    }
+
+    public ChatMessage() {
+
+    }
+}
--- app/src/main/java/com/damask/chattr/MainActivity.java
+++ app/src/main/java/com/damask/chattr/MainActivity.java
@@ -2,16 +2,64 @@ package com.damask.chattr;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ListView lv;
+    private Firebase rootRef;
+    private MyAdapter ad;
+    ArrayList<ChatMessage> messenger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messenger = new ArrayList<ChatMessage>();
+        ad = new MyAdapter();
+        lv = (ListView) findViewById(R.id.messages);
+        lv.setAdapter(ad);
+
+        Firebase.setAndroidContext(this);
+        rootRef = new Firebase("https://brilliant-inferno-9405.firebaseio.com");
+
+        rootRef.child("messages").keepSynced(true);
+        rootRef.child("messages").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                ChatMessage current;
+                ArrayList<ChatMessage> msgs = new ArrayList<ChatMessage>((int) (dataSnapshot.getChildrenCount()));
+                for (DataSnapshot m : dataSnapshot.getChildren()) {
+                    current = m.getValue(ChatMessage.class);
+                    messenger.add(current);
+                    Log.i("User", current.getmText());
+                }
+                ad.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+
     }
 
     @Override
@@ -35,4 +83,46 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void sendMessage(View v) {
+        EditText textField = (EditText) findViewById(R.id.textField);
+        String message = textField.getText().toString();
+
+        if (message.length()>0) {
+            rootRef.child("messages").push().setValue(new ChatMessage("Damask", message));
+            textField.setText("");
+        }
+    }
+
+
+    public class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return messenger.size();
+        }
+        @Override
+        public Object getItem(int position) {
+            return messenger.get(position%messenger.size());
+        }
+        @Override
+        public long getItemId(int position) {
+            return position % messenger.size();
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv;
+            if(convertView!=null ){
+                tv=(TextView)convertView;
+            }else {
+                tv = new TextView(MainActivity.this);
+                tv.setTextSize(15);
+                tv.setPadding(5, 5, 5, 5);
+            }
+            String user = messenger.get(position%messenger.size()).getmUser();
+            String messageBody = messenger.get(position%messenger.size()).getmText();
+            tv.setText(user + ": " + messageBody);
+            return tv;
+        }
+    }
+
 }
